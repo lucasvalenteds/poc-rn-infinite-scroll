bd37e9bd0feb95c34ed43d836da1eb8f9c0d056f
Lucas <lucasvalenteds@gmail.com>
2020-03-14 18:36:14 -0300

    Add unit test

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,16 +1,36 @@
-import {ScrollView} from 'react-native';
 import React from 'react';
-import App from './App';
-
+import {FlatList} from 'react-native';
 import ReactRenderer, {
-  ReactTestRenderer,
-  ReactTestInstance,
+    ReactTestRenderer,
+    ReactTestInstance,
 } from 'react-test-renderer';
+import Axios from 'axios';
+import AxiosMockAdapter from 'axios-mock-adapter';
+import App, {Notification} from './App';
 
-test('It renders the text', () => {
-  const renderer: ReactTestRenderer = ReactRenderer.create(<App />);
+function createNotification(): Notification {
+    return {
+        id: "id-" + Math.random(),
+        title: "Title",
+        content: "Content",
+    };
+}
 
-  const node: ReactTestInstance = renderer.root.findByType(ScrollView);
+function createNotifications(amount: number): Notification[] {
+    return Array(amount).fill(0).map(createNotification);
+}
 
-  expect(node).toBeDefined();
+test('It renders items on first load', async () => {
+    const server = new AxiosMockAdapter(Axios);
+    server.onGet("http://localhost:8080/?offset=0").reply(200, {
+        notifications: createNotifications(10),
+    });
+    const renderer: ReactTestRenderer = ReactRenderer.create(<App />);
+
+    await ReactRenderer.act(async () => {});
+
+    const node: ReactTestInstance = renderer.root.findByType(FlatList);
+
+    expect(node.props.data).toHaveLength(10);
+    expect(server.history.get).toHaveLength(1);
 });
