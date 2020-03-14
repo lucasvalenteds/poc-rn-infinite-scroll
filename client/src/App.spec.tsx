deba3617b23510d02a55d5f2a5778e759606ed41
Lucas <lucasvalenteds@gmail.com>
2020-03-14 19:30:56 -0300

    Fixes code style issues

--- client/src/App.spec.tsx
+++ client/src/App.spec.tsx
@@ -1,36 +1,38 @@
 import React from 'react';
 import {FlatList} from 'react-native';
 import ReactRenderer, {
-    ReactTestRenderer,
-    ReactTestInstance,
+  ReactTestRenderer,
+  ReactTestInstance,
 } from 'react-test-renderer';
 import Axios from 'axios';
 import AxiosMockAdapter from 'axios-mock-adapter';
 import App, {Notification} from './App';
 
 function createNotification(): Notification {
-    return {
-        id: "id-" + Math.random(),
-        title: "Title",
-        content: "Content",
-    };
+  return {
+    id: 'id-' + Math.random(),
+    title: 'Title',
+    content: 'Content',
+  };
 }
 
 function createNotifications(amount: number): Notification[] {
-    return Array(amount).fill(0).map(createNotification);
+  return Array(amount)
+    .fill(0)
+    .map(createNotification);
 }
 
 test('It renders items on first load', async () => {
-    const server = new AxiosMockAdapter(Axios);
-    server.onGet("http://localhost:8080/?offset=0").reply(200, {
-        notifications: createNotifications(10),
-    });
-    const renderer: ReactTestRenderer = ReactRenderer.create(<App />);
+  const server = new AxiosMockAdapter(Axios);
+  server.onGet('http://localhost:8080/?offset=0').reply(200, {
+    notifications: createNotifications(10),
+  });
+  const renderer: ReactTestRenderer = ReactRenderer.create(<App />);
 
-    await ReactRenderer.act(async () => {});
+  await ReactRenderer.act(async () => {});
 
-    const node: ReactTestInstance = renderer.root.findByType(FlatList);
+  const node: ReactTestInstance = renderer.root.findByType(FlatList);
 
-    expect(node.props.data).toHaveLength(10);
-    expect(server.history.get).toHaveLength(1);
+  expect(node.props.data).toHaveLength(10);
+  expect(server.history.get).toHaveLength(1);
 });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,82 +7,82 @@ import {
   View,
   TouchableNativeFeedback,
   StyleSheet,
-  ListRenderItemInfo,
 } from 'react-native';
 import Axios, {AxiosResponse} from 'axios';
 
 export interface Notification {
-    id: string;
-    title: string;
-    content: string;
+  id: string;
+  title: string;
+  content: string;
 }
 
 export interface Notifications {
-    notifications: Notification[];
+  notifications: Notification[];
 }
 
-export const NotificationListItem: React.FC<Notification> = (props): React.ReactElement => {
-    const style = StyleSheet.create({
-        notification: {
-            width: "100%",
-            paddingHorizontal: 16,
-            paddingVertical: 16,
-        },
-        title: {
-            fontSize: 16,
-            fontWeight: "bold",
-        },
-        content: {
-            fontSize: 16,
-            fontStyle: "normal",
-        },
-    });
+export const NotificationListItem: React.FC<Notification> = (
+  props,
+): React.ReactElement => {
+  const style = StyleSheet.create({
+    notification: {
+      width: '100%',
+      paddingHorizontal: 16,
+      paddingVertical: 16,
+    },
+    title: {
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+    content: {
+      fontSize: 16,
+      fontStyle: 'normal',
+    },
+  });
 
-    return (
-        <TouchableNativeFeedback>
-            <View key={props.id} style={style.notification}>
-                <Text style={style.title}>{props.title}</Text>
-                <Text
-                    ellipsizeMode="tail"
-                    numberOfLines={2}
-                    style={style.content}
-                >{props.content}</Text>
-            </View>
-        </TouchableNativeFeedback>
-    );
-}
+  return (
+    <TouchableNativeFeedback>
+      <View key={props.id} style={style.notification}>
+        <Text style={style.title}>{props.title}</Text>
+        <Text ellipsizeMode="tail" numberOfLines={2} style={style.content}>
+          {props.content}
+        </Text>
+      </View>
+    </TouchableNativeFeedback>
+  );
+};
 
 const App: React.FC = (): React.ReactElement => {
   const [offset, setOffset] = useState<number>(0);
   const [notifications, setNotifications] = useState<Notification[]>([]);
 
   useEffect(() => {
-      if (offset >= 50) {
-          return;
-      }
+    if (offset >= 50) {
+      return;
+    }
 
-      console.debug('Offset changed', offset);
+    console.debug('Offset changed', offset);
 
-      Axios.get("http://localhost:8080/?offset=" + offset)
-          .then((response: AxiosResponse<Notifications>) => {
-              setNotifications((previous: Notification[]) => [
-                  ...previous,
-                  ...response.data.notifications,
-              ]);
-          });
+    Axios.get('http://localhost:8080/?offset=' + offset).then(
+      (response: AxiosResponse<Notifications>) => {
+        setNotifications((previous: Notification[]) => [
+          ...previous,
+          ...response.data.notifications,
+        ]);
+      },
+    );
   }, [offset]);
 
   return (
     <>
       <StatusBar />
       <SafeAreaView>
-          <FlatList
-              data={notifications}
-              keyExtractor={(item) => item.id}
-              renderItem={(props) => NotificationListItem(props.item)}
-              onEndReached={() => setOffset((previous: number) => previous + 10)}
-              onEndReachedThreshold={0.2}
-          />
+        <FlatList
+          data={notifications}
+          keyExtractor={item => item.id}
+          renderItem={props => NotificationListItem(props.item)}
+          onEndReached={() => setOffset((previous: number) => previous + 10)}
+          onEndReachedThreshold={0.2}
+        />
       </SafeAreaView>
     </>
   );
