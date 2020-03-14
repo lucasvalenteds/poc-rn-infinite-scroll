5187a5e91b875d18ddf3ce2b9caf33c1c7db3c11
Lucas <lucasvalenteds@gmail.com>
2020-03-14 17:25:30 -0300

    Implements notifications fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,70 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
+  FlatList,
+  Text,
+  View,
+  TouchableNativeFeedback,
+  StyleSheet,
 } from 'react-native';
+import Axios, {AxiosResponse} from 'axios';
+
+export interface Notification {
+    id: string;
+    title: string;
+    content: string;
+}
+
+export interface Notifications {
+    notifications: Notification[];
+}
+
+export const NotificationListItem: React.FC<Notification> = (props): React.ReactElement => {
+    const style = StyleSheet.create({
+        notification: {
+            width: "100%",
+            paddingHorizontal: 16,
+            paddingVertical: 16,
+        },
+        title: {
+            fontSize: 22,
+        },
+        content: {
+            fontSize: 16,
+        },
+    });
+
+    return (
+        <TouchableNativeFeedback>
+            <View key={props.id} style={style.notification}>
+                <Text style={style.title}>{props.title}</Text>
+                <Text style={style.content}>{props.content}</Text>
+            </View>
+        </TouchableNativeFeedback>
+    );
+}
 
 const App: React.FC = (): React.ReactElement => {
+  const [offset, setOffset] = useState<number>(0);
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+
+  useEffect(() => {
+      Axios.get("http://localhost:8080/?offset=" + offset)
+          .then((response: AxiosResponse<Notifications>) => {
+              setNotifications(response.data.notifications)
+          });
+  }, [offset]);
+
   return (
     <>
       <StatusBar />
       <SafeAreaView>
-        <ScrollView>
-        </ScrollView>
+            <FlatList
+                data={notifications}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => NotificationListItem(item)}
+            />
       </SafeAreaView>
     </>
   );
