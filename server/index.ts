2c67ab5dad6ec4babfb0d2b7b5c4807ee648f9c4
Lucas <lucasvalenteds@gmail.com>
2020-03-14 19:29:42 -0300

    Fixes code style issues

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
-import {Server} from './src/server';
+import { Server } from "./src/server";
 
 const port = 8080;
 
-Server.listen(port)
-.on("listening", () => console.debug('Server running on port ' + port));
+Server.listen(port).on("listening", () =>
+  console.debug("Server running on port " + port)
+);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,32 @@
-import * as Http from 'http';
-import * as Url from 'url';
-import * as QueryString from 'querystring';
-import * as UUID from 'uuid';
-import Faker from 'faker';
+import * as Http from "http";
+import * as Url from "url";
+import * as QueryString from "querystring";
+import * as UUID from "uuid";
+import Faker from "faker";
 
 export interface Notification {
-    id: string;
-    title: string;
-    content: string;
+  id: string;
+  title: string;
+  content: string;
 }
 
 export const Server = Http.createServer((request, response) => {
-    const url: QueryString.ParsedUrlQuery = Url.parse((request.url as string), true).query;
+  const url: QueryString.ParsedUrlQuery = Url.parse(request.url as string, true)
+    .query;
 
-    const {offset} = url;
+  const { offset } = url;
 
-    const notifications: Notification[] = Array(10).fill(0)
-    .map((_, index) => index + parseInt((offset as string) || '0', 10))
-    .map((offset) => ({
-        id: UUID.v4(),
-        title: `${offset} - ${Faker.lorem.words(3)}`,
-        content: Faker.lorem.paragraph(1),
+  const notifications: Notification[] = Array(10)
+    .fill(0)
+    .map((_, index) => index + parseInt((offset as string) || "0", 10))
+    .map(offset => ({
+      id: UUID.v4(),
+      title: `${offset} - ${Faker.lorem.words(3)}`,
+      content: Faker.lorem.paragraph(1)
     }));
 
-    response.setHeader('Content-Type', 'application/json');
-    response.statusCode = 200;
-    response.write(JSON.stringify({ notifications }));
-    response.end();
+  response.setHeader("Content-Type", "application/json");
+  response.statusCode = 200;
+  response.write(JSON.stringify({ notifications }));
+  response.end();
 });
